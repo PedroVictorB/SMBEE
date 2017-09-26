2b84b0f6ce837f80194bd79f3bbc8b3b5f2662d5
pedrovb <pedrovictor_caldas@hotmail.com>
2017-09-26 10:56:15 -0300

    first commit

---
+++ src/main/java/org/ufrn/imd/bpmn/smbee/App.java
@@ -0,0 +1,13 @@
+package org.ufrn.imd.bpmn.smbee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
